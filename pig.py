b9dd8d041c1253997d91b325266ec1f5759c6b19
Radek Molenda <radek.molenda@onthebeach.co.uk>
2014-04-28 11:19:34 +0100

    Automode :pig:

--- pig.py
+++ pig.py
@@ -23,29 +23,45 @@ class Pig(QLabel):
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setAutoFillBackground(False)
         self.voice = Snorter(self)
+        self.autoMode = False
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.randomMove)
 
     def moveRight(self):
+        self.snort()
         self.setPixmap(pigR)
         self.move(self.pos() + QPoint(1, 0))
 
     def moveLeft(self):
+        self.snort()
         self.setPixmap(pigL)
         self.move(self.pos() + QPoint(-1, 0))
 
+    def randomMove(self):
+        action = random.choice([self.moveRight, self.moveLeft])
+        action()
+
     def snort(self):
         self.voice.snort()
+
+    def toggleAutomode(self):
+        self.autoMode = not self.autoMode
+        if self.autoMode:
+            self.timer.start(1000)
+        else:
+            self.timer.stop()
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Escape:
             app.quit()
         elif e.key() == Qt.Key_Right:
-            self.snort()
             self.moveRight()
         elif e.key() == Qt.Key_Left:
-            self.snort()
             self.moveLeft()
         elif e.key() == Qt.Key_Return:
             self.snort()
+        elif e.key() == Qt.Key_A:
+            self.toggleAutomode()
 
 class Snorter():
     def __init__(self, pig):
